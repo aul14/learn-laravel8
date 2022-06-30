c8fe5ec4ea07dfaa5cdb72aa6d5f6ea831b0c8c2
aul14 <rahmanaulia336@gmail.com>
2022-06-30 11:03:34 +0700

    learn controller & collection

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,5 @@
     <h1>Halaman About</h1>
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" alt="Aul">
+    <img src="img/{{ $image }}" alt="Aul" class="img img-thumbnail rounded-circle">
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,64 +32,7 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug'  => 'judul-post-pertama',
-            'author' => 'Aul',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, quis est modi consequuntur dolore beatae eos voluptas animi mollitia ut ab, repellat dicta architecto molestiae veritatis eveniet nobis cum, doloribus odio. Vero sapiente necessitatibus nobis eligendi debitis corporis vitae sint reiciendis, aliquam, aperiam quas illum dicta nemo placeat excepturi voluptates unde ullam dolorum impedit incidunt aspernatur velit quod explicabo! Reiciendis et animi provident quasi ea quisquam nostrum rem fugit quas velit reprehenderit culpa a, nam quis quod alias ex accusantium.',
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug'  => 'judul-post-kedua',
-            'author' => 'Rahman',
-            'body' => '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus quibusdam cupiditate, magnam numquam
-            aperiam corrupti dicta officiis quam suscipit consequuntur repellat dolor nesciunt qui reiciendis fuga illum ullam
-            iste, dolorum ab. Reiciendis deleniti deserunt velit alias placeat eos possimus dolorum. Porro deleniti architecto
-            molestiae possimus inventore magni maxime sapiente corrupti adipisci, consectetur repellendus tempore quasi, sint,
-            molestias officia. Veniam itaque ex molestiae adipisci odio! Sit, illum consectetur. Ullam labore, numquam
-            cupiditate, enim quasi aliquid dicta iure excepturi assumenda non amet quae corrupti. Commodi error nisi optio atque
-            sapiente quis facilis veniam facere porro eaque voluptatem, inventore illo laudantium assumenda?',
-        ],
-    ];
-
-    return view('posts', [
-        'title' => 'Blog',
-        'posts' => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // Halaman single post
-Route::get('/posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            'title' => 'Judul Post Pertama',
-            'slug'  => 'judul-post-pertama',
-            'author' => 'Aul',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, quis est modi consequuntur dolore beatae eos voluptas animi mollitia ut ab, repellat dicta architecto molestiae veritatis eveniet nobis cum, doloribus odio. Vero sapiente necessitatibus nobis eligendi debitis corporis vitae sint reiciendis, aliquam, aperiam quas illum dicta nemo placeat excepturi voluptates unde ullam dolorum impedit incidunt aspernatur velit quod explicabo! Reiciendis et animi provident quasi ea quisquam nostrum rem fugit quas velit reprehenderit culpa a, nam quis quod alias ex accusantium.',
-        ],
-        [
-            'title' => 'Judul Post Kedua',
-            'slug'  => 'judul-post-kedua',
-            'author' => 'Rahman',
-            'body' => '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus accusamus quibusdam cupiditate, magnam numquam
-            aperiam corrupti dicta officiis quam suscipit consequuntur repellat dolor nesciunt qui reiciendis fuga illum ullam
-            iste, dolorum ab. Reiciendis deleniti deserunt velit alias placeat eos possimus dolorum. Porro deleniti architecto
-            molestiae possimus inventore magni maxime sapiente corrupti adipisci, consectetur repellendus tempore quasi, sint,
-            molestias officia. Veniam itaque ex molestiae adipisci odio! Sit, illum consectetur. Ullam labore, numquam
-            cupiditate, enim quasi aliquid dicta iure excepturi assumenda non amet quae corrupti. Commodi error nisi optio atque
-            sapiente quis facilis veniam facere porro eaque voluptatem, inventore illo laudantium assumenda?',
-        ],
-    ];
-    $new_post = [];
-    foreach ($blog_posts as $key => $post) {
-        if ($post["slug"] == $slug) {
-            $new_post = $post;
-        }
-    }
-    return view('post', [
-        'title' => 'Single Post',
-        'post'  => $new_post
-    ]);
-});
+Route::get('/posts/{slug}', [PostController::class, 'show']);
