a3504c35ea90ef9eaa0a686efeafaaec98004d0b
aul14 <rahmanaulia336@gmail.com>
2022-07-08 10:52:08 +0700

    learn create registration with laravel 8

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,26 @@ class RegisterController extends Controller
             'title'     => 'Register',
             'active'    => 'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name'          => 'required|max:100',
+            'username'      => ['required', 'min:3', 'max:100', 'unique:users,username'],
+            'email'         => 'required|email:dns|unique:users,email',
+            'password'      => 'required|min:5|max:100'
+        ]);
+
+        // $validateData['password'] = bcrypt($validateData['password']);
+        $validateData['password'] = Hash::make($validateData['password']);
+
+        // dd($validateData);
+
+        User::create($validateData);
+
+        // $request->session()->flash('sukses', 'Registration successfull! Please login');
+
+        return redirect('/login')->with('sukses', 'Registration successfull! Please login');
     }
 }
